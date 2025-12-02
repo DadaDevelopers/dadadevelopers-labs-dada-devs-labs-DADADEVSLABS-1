548a0ff3b6af45806f38f069297cb2892e7cd3ac
Findy <favouritefindy@gmail.com>
2025-12-02 03:23:44 +0100

    chore: Setup AuthProvider context and API initialization

--- frontend/DirectAid/src/main.tsx
+++ frontend/DirectAid/src/main.tsx
@@ -1,21 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App.tsx";
-import { AuthProvider } from "./hooks/useAuth";
 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { AuthProvider } from './contexts/AuthContext.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App.tsx'
 // 1. Initialize a new QueryClient instance
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
           <App />
-        </AuthProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
-  </StrictMode>
-);
+        </QueryClientProvider>
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>,
+)
