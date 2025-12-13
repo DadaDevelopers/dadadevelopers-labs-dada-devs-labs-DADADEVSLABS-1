ff19466d58bf80f95c00207facb9eb872f2a3186
Bridgette M. Serah <bridgettemwaniki@gmail.com>
2025-12-13 13:21:47 +0300

    Update landing page layout and add testimonial/footer public

--- lightning-chama/src/app/landing-page/page.tsx
+++ lightning-chama/src/app/landing-page/page.tsx
@@ -1,12 +1,17 @@
-"use client"
+"use client";
+
 import WhyChooseChama from "@/app/landing-page/WhyChooseChama";
 import WhyBitcoin from "@/app/landing-page/WhyBitcoin";
+import Testimonials from "@/app/landing-page/Testimonials";
+import Footer from "@/app/landing-page/Footer";
 
 export default function Page() {
   return (
-    <main className="w-full">
+     <main className="min-h-screen w-full overflow-x-hidden bg-white">
       <WhyChooseChama />
       <WhyBitcoin />
+      <Testimonials />
+      <Footer />
     </main>
   );
 }
